e200bd73aa91cb3def507f43459767892cd674f2
Mathew Merry <mat.merry@outlook.com>
2026-07-15 14:30:58 +0100

    Created a class for the CAN frames

---
+++ src/CANFrame.py
@@ -0,0 +1,40 @@
+MAX_DATA_LENGTH = 8
+
+STANDARD_MASK = 0x7FF
+EXTENDED_MASK = 0x1FFFFFFF
+
+
+class CANFrame:
+    """A class representing a CAN frame"""
+
+    def __init__(self, can_id: int, can_data: bytes = b"", is_extended: bool = False, is_rtr: bool = False) -> None:
+        """Constructs the data needed for a CAN frame
+        Args:
+            can_id (int): CAN frame id
+            can_data (bytes, optional): CAN frame data. Defaults to empty bytes.
+            is_extended (bool, optional): Whether the CAN frame is extended. Defaults to False.
+            is_rtr (bool, optional): Whether the CAN frame is a remote transmition request. Defaults to False."""
+        self.is_extended = is_extended
+        self.is_rtr = is_rtr
+        self.can_id = can_id
+        self.can_data = can_data
+
+
+    @property
+    def can_id(self) -> int:
+        return self._can_id
+
+    @can_id.setter
+    def can_id(self, can_id: int) -> None:
+        self._can_id = can_id & (EXTENDED_MASK if self.is_extended else STANDARD_MASK)
+
+    @property
+    def can_data(self) -> bytes:
+        return self._can_data
+
+    @can_data.setter
+    def can_data(self, can_data: bytes) -> None:
+        if len(can_data) > MAX_DATA_LENGTH:
+            raise ValueError(f"CAN frame data cannot exceed {MAX_DATA_LENGTH} bytes, provided payload was {len(can_data)} bytes")
+        self._can_data = can_data
+        self.dlc = len(can_data)
