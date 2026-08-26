9c10571a4abd3e728551d33b526be2c4807c9e20
Mathew Merry <mat.merry@outlook.com>
2026-08-26 17:10:26 +0100

    Added comments to explain whats happening in the demo

--- demo.py
+++ demo.py
@@ -2,10 +2,17 @@ from src import *
 
 
 if __name__ == "__main__":
+    #Initialize the adapter with the correct port
     adapter = WaveshareCan("COM6")
+
+    # Update necessary configurations (fixed is default)
     adapter.update_configurations(communication_type=Type.FIXED)
+
+    # Create a CAN Frame and send it
     frame = CANFrame(123, b"\x12\x34\x45")
     adapter.send_frame(frame)
+
+    # Continuously read incoming frames from the bus
     while True:
         received_frame = adapter.read_frame()
         print(received_frame)
